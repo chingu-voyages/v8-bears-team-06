75b359ba021ac40cfd246c39281b777d187ec0c4
Otsuki Hitoshi <hitochan777@gmail.com>
2019-03-18 20:57:28 +0900

    Add apollo client

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,3 +1,41 @@
 import React from 'react';
+import {Query} from 'react-apollo';
+import gql from 'graphql-tag';
 
-export default () => <div>it works!</div>;
+import {User} from '../components/User';
+
+export const allUsersQuery = gql`
+  {
+    users {
+      id
+      email
+    }
+  }
+`;
+
+export default () => (
+  <Query query={allUsersQuery}>
+    {({loading, error, data}) => {
+      if (error) {
+        return <div>Could not fetch users</div>;
+      }
+
+      if (loading) {
+        return <div>Loading...</div>;
+      }
+
+      return (
+        <section>
+          <h1>List of Users</h1>
+          <ul>
+            {data.users.map(user => (
+              <li>
+                <User key={user.id} email={user.email} />
+              </li>
+            ))}
+          </ul>
+        </section>
+      );
+    }}
+  </Query>
+);
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import {ApolloServer, gql} from 'apollo-server-express';
 import next from 'next';
 
@@ -18,9 +19,16 @@ export default async function main() {
   await nextApp.prepare();
 
   const server = express();
+  server.use(bodyParser.json());
+  server.use(bodyParser.urlencoded({extended: true}));
 
   const apolloServer = createApolloServer();
   apolloServer.applyMiddleware({app: server});
+
+  // all requests to paths other than `/graphql` are processed by Nextjs
+  server.get('*', (req, res) => {
+    return nextHandler(req, res);
+  });
 
   try {
     await setupDbConnection(dev);
@@ -31,11 +39,6 @@ export default async function main() {
     console.error(err);
     return;
   }
-
-  // all requests to paths other than `/graphql` are processed by Nextjs
-  server.get('*', (req, res) => {
-    return nextHandler(req, res);
-  });
 
   server.listen(port, err => {
     if (err) {

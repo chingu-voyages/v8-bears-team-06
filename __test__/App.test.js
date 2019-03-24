66e8f076dfb4f9790c7c659371128bf8870731a1
Will Ward <will.j.ward3@gmail.com>
2019-03-24 16:47:26 +0900

    Commit updates to __test__/App.test.js and jest.config.js

--- __test__/App.test.js
+++ __test__/App.test.js
@@ -5,17 +5,17 @@ import renderer from "react-test-renderer";
 import App from "../client/components/App";
 
 describe("With Enzyme", () => {
-    it('App shows "Hello, you Chingu beauty!"', () => {
-        const app = shallow(<App />);
+  it('App shows "Hello, you Chingu beauty!"', () => {
+    const app = shallow(<App />);
 
-        expect(app.find("div").text()).toEqual("Hello, you Chingu beauty!");
-    });
+    expect(app.find("div").text()).toEqual("Hello, you Chingu beauty!");
+  });
 });
 
 describe("With Snapshot Testing", () => {
-    it('App shows "Hello, you Chingu beauty!"', () => {
-        const component = renderer.create(<App />);
-        const tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+  it('App shows "Hello, you Chingu beauty!"', () => {
+    const component = renderer.create(<App />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });

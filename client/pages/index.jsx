5abd8ce28081dd05dde80768448f7d0bdd45d649
Allan Applebee <allan.applebee@gmail.com>
2019-03-26 18:49:43 +1100

    homepage fairly finished, CSS a bit hacky!

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -4,9 +4,12 @@ import Layout from '../components/Layout'
 const Index = () => (
   <Layout>
     <div className="bg">
-      <h1>goodWork</h1>
-      <p>Where charities and volunteers meet!</p>
-
+        <h1>goodWork</h1>
+        <h3>Where charities and volunteers meet!</h3>
+        <div className="buttons">
+          <button type="button" className="btn btn-success">I'm a charity</button>
+          <button type="button" className="btn btn-outline-success">I'm a volunteer</button>
+        </div>
     </div>
     <style>{`
       body, html {
@@ -14,23 +17,39 @@ const Index = () => (
         width: 100vw;
       }
 
-      .bg .h1 {
-          font-size: 64px;
-          font-weight: 300;
-          margin: 80px 0 25px;
-          color: #2c3e50;
-          text-align: center;
+      .buttons {
+        flex-direction: row;
+      }
+
+      .btn-outline-success {
+        margin-left: 30px;
+      }
+
+      h1 {
+        font-size: 64px;
+        font-weight: 700;
+        margin: 0 0 125px 0;
+        color: #2c3e50;
+      }
+
+      h3 {
+        color: #2c3e50;
+        margin-bottom: 40px;
       }
 
       .bg {
-        background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(../static/leone.jpg);
-        height: calc(100vh - 56px);
+        display: flex;
+        flex-direction: column;
+        background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(../static/leone.jpg);
+        height: 100vh;
         min-height: 500px;
         width: 100vw;
         background-position: center;
         background-repeat: no-repeat;
         background-size: cover;
         position: relative;
+        justify-content: center;
+        text-align: center;
       }
     `}</style>
     

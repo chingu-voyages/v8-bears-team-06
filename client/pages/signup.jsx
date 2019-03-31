aed6d0b960c191606918b67bca205ec79b7ac180
Will Ward <will.j.ward3@gmail.com>
2019-03-31 22:32:25 +0900

    Added some stylings to user registration form

--- client/pages/signup.jsx
+++ client/pages/signup.jsx
@@ -21,29 +21,54 @@ export default () => {
         <Layout>
           <form className="form-signin mt-5">
             <h1 className="mb-3">Sign Up</h1>
-            <input
-              type="email"
-              className="form-control mb-3"
-              id="email"
-              placeholder="Email"
-              value={email}
-              onChange={event => {
-                setEmail(event.target.value);
-              }}
-              autoFocus
-              required
-            />
-            <input
-              type="password"
-              className="form-control mb-3"
-              id="password"
-              placeholder="Password"
-              value={password}
-              onChange={event => {
-                setPassword(event.target.value);
-              }}
-              required
-            />
+            <div className="form-group">
+              <label htmlFor="email" className="float-left required">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={event => {
+                  setEmail(event.target.value);
+                }}
+                autoFocus
+                required
+              />
+              <small
+                id="emailHelp"
+                className="form-text text-muted float-left mb-3"
+              >
+                We&apos;ll never share your email with anyone else.
+              </small>
+              <br />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password" className="float-left required">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={event => {
+                  setPassword(event.target.value);
+                }}
+                required
+              />
+              <small
+                id="emailHelp"
+                className="form-text text-muted float-left mb-3 d-inline-block"
+              >
+                Must contain at least 6 alphanumeric characters.
+              </small>
+              <br />
+            </div>
+
             <button
               type="submit"
               className="btn btn-primary float-left"
@@ -72,6 +97,12 @@ export default () => {
 
         .form-signin .form-control {
           padding: 10px;
+        }
+
+        .required:after{ 
+          content:'*'; 
+          color:red; 
+          padding-left:5px;
         }
       `}</style>
         </Layout>

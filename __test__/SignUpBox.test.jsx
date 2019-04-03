8866a84be4a8716cfa56bca3773d89e30b7499ce
Will Ward <will.j.ward3@gmail.com>
2019-04-03 14:27:25 +0900

    ran format

--- __test__/SignUpBox.test.jsx
+++ __test__/SignUpBox.test.jsx
@@ -5,17 +5,17 @@ import renderer from "react-test-renderer";
 import SignUp from "../client/components/SignUpBox";
 
 describe("With Enzyme", () => {
-    it('SignUpBox shows "Sign Up"', () => {
-        const signup = shallow(<SignUp />);
+  it('SignUpBox shows "Sign Up"', () => {
+    const signup = shallow(<SignUp />);
 
-        expect(signup.find("h1").text()).toEqual("Sign Up");
-    });
+    expect(signup.find("h1").text()).toEqual("Sign Up");
+  });
 });
 
 describe("With Snapshot Testing", () => {
-    it('SignUpBox shows "Sign Up"', () => {
-        const component = renderer.create(<SignUp />);
-        const tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+  it('SignUpBox shows "Sign Up"', () => {
+    const component = renderer.create(<SignUp />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });

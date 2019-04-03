e1f97e8d580125dc15359258218e0421e2c74106
Allan Applebee <allan.applebee@gmail.com>
2019-04-03 18:55:51 +1100

    basic layout finished

--- client/pages/howitworks.jsx
+++ client/pages/howitworks.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../components/layouts/Layout'
+import Layout from '../components/layouts/Layout.jsx'
 
 const HowItWorks = () => (
     <Layout>
@@ -7,7 +7,71 @@ const HowItWorks = () => (
             <div className="jumbotron">
                 <h1>How it works</h1>
             </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-12 col-sm-12 col-md-6">
+                        <div className="row info-row">
+                            <div><h1>Step 1</h1></div>
+                            <div>
+                                <p>Maybe we better talk out here; the observation lounge has turned into a swamp. When has justice ever been as simple as a rule book? We could cause a diplomatic crisis. Take the ship into the Neutral Zone Commander William Riker of the Starship Enterprise. Besides, you look good in a dress.</p>
+                            </div>
+                        </div>
+                        <div className="row info-row">
+                            <div><h1>Step 2</h1></div>
+                            <div>
+                                <p>Maybe we better talk out here; the observation lounge has turned into a swamp. When has justice ever been as simple as a rule book? We could cause a diplomatic crisis. Take the ship into the Neutral Zone Commander William Riker of the Starship Enterprise. Besides, you look good in a dress.</p>
+                            </div>
+                        </div>
+                        <div className="row info-row">
+                            <div><h1>Step 3</h1></div>
+                            <div>
+                                <p>Maybe we better talk out here; the observation lounge has turned into a swamp. When has justice ever been as simple as a rule book? We could cause a diplomatic crisis. Take the ship into the Neutral Zone Commander William Riker of the Starship Enterprise. Besides, you look good in a dress.</p>
+                            </div>
+                        </div>
+                        <div className="row info-row">
+                            <div><h1>Step 4</h1></div>
+                            <div>
+                                <p>Maybe we better talk out here; the observation lounge has turned into a swamp. When has justice ever been as simple as a rule book? We could cause a diplomatic crisis. Take the ship into the Neutral Zone Commander William Riker of the Starship Enterprise. Besides, you look good in a dress.</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    
+                    <div className="col-xs-12 col-sm-12 col-md-6">
+                        <div className="row info-row">
+                            <div><h1>Step 1</h1></div>
+                            <div>
+                                <p>ake the ship into the Neutral Zone Commander William Riker of the Starship Enterprise. Besides, you look good in a dress.</p>
+                            </div>
+                        </div>
+                        <div className="row info-row">
+                            <div><h1>Step 2</h1></div>
+                            <div>
+                                <p>Maybe we better talk out here; the observation lounge has turned into a swamp. When has justice ever been as simple as a rule book? We could cause a diplomatic crisis. Take the ship into the Neutral Zone Commander William Riker of the Starship Enterprise. Besides, you look good in a dress.</p>
+                            </div>
+                        </div>
+                        <div className="row info-row">
+                            <div><h1>Step 3</h1></div>
+                            <div>
+                                <p>Maybe we better talk out here; the observation lounge has turned into a swamp. When has justice ever been as simple as a rule book? We could cause a diplomatic crisis. Take the ship into the Neutral Zone Commander William Riker of the Starship Enterprise. Besides, you look good in a dress.</p>
+                            </div>
+                        </div>
+                        <div className="row info-row">
+                            <div><h1>Step 4</h1></div>
+                            <div>
+                                <p>Maybe we better talk out here; the observation lounge has turned into a swamp. When has justice ever been as simple as a rule book? We could cause a diplomatic crisis. Take the ship into the Neutral Zone Commander William Riker of the Starship Enterprise. Besides, you look good in a dress.</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
+        <style>{`
+            // .container{ border: solid black 1px; height: 100%; }
+            .col { width: 50%; height: 100%; padding: 10px 10px 10px 10px; }
+            .info-row { height: 200px; }
+        `}</style>
     </Layout>
 )
 

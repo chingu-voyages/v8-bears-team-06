a840deb9bed61437dbb601c9f3ef9367e666e143
Will Ward <will.j.ward3@gmail.com>
2019-04-03 13:25:13 +0900

    added text check for SignUpBox with Enzyme

---
+++ __test__/SignUpBox.test.jsx
@@ -0,0 +1,21 @@
+import { shallow } from "enzyme";
+import React from "react";
+import renderer from "react-test-renderer";
+
+import SignUp from "../client/components/SignUpBox";
+
+describe("With Enzyme", () => {
+    it('SignUpBox shows "Sign Up"', () => {
+        const signup = shallow(<SignUp />);
+
+        expect(signup.find("h1").text()).toEqual("Sign Up");
+    });
+});
+
+describe("With Snapshot Testing", () => {
+    it('SignUpBox shows "Sign Up"', () => {
+        const component = renderer.create(<SignUp />);
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});

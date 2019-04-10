e96eb1ff8b223ddd17f3b1f11aea0aff0a94df3c
Allan Applebee <allan.applebee@gmail.com>
2019-04-11 07:49:28 +1000

    work schema added

---
+++ server/models/work.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  title: String,
+  startDate: String,
+  endDate: String,
+  description: String,
+  thoughts: String,
+  link: String,
+  author: {
+      id: {
+          type: mongoose.schema.Types.ObjectID,
+          ref: "User"
+      },
+      userName: String
+  }
+});
+
+export const Work = mongoose.model("Work", schema);

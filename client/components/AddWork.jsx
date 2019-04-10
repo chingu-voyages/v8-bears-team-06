60af6c4f79b70a8be22a8feb3f12771874215be0
Allan Applebee <allan.applebee@gmail.com>
2019-04-10 21:53:54 +1000

    basic form finished, bootstrap needs adding

--- client/components/AddWork.jsx
+++ client/components/AddWork.jsx
@@ -31,6 +31,99 @@ const AddWork = ({ apolloClient }) =>{
                 />
                 <br />
                 </div>
+
+                <div className="form-group">
+                <label htmlFor="start-date" className="float-left required">
+                    Start date
+                </label>
+                <input 
+                    type="date"
+                    className="form-control"
+                    id="start-date"
+                    placeholder="dd/mm/yyyy"
+                    value={startDate}
+                    onChange={event => { 
+                        setStartDate(event.target.value); 
+                    }}
+                    autoFocus
+                    required
+                />
+                <br />
+                </div>
+
+                <div className="form-group">
+                <label htmlFor="end-date" className="float-left required">
+                    End date
+                </label>
+                <input 
+                    type="date"
+                    className="form-control"
+                    id="end-date"
+                    placeholder="dd/mm/yyyy"
+                    value={endDate}
+                    onChange={event => { 
+                        setEndDate(event.target.value); 
+                    }}
+                    autoFocus
+                    required
+                />
+                <br />
+                </div>
+
+                <div className="form-group">
+                <label htmlFor="description" className="float-left required">
+                    Description of project
+                </label>
+                <input 
+                    type="text"
+                    className="form-control"
+                    id="description"
+                    placeholder="description of your project..."
+                    value={description}
+                    onChange={event => { 
+                        setDescription(event.target.value); 
+                    }}
+                    autoFocus
+                    required
+                />
+                <br />
+                </div>
+
+                <div className="form-group">
+                <label htmlFor="thoughts" className="float-left">
+                    Thoughts
+                </label>
+                <input 
+                    type="text"
+                    className="form-control"
+                    id="thoughts"
+                    placeholder="Your thoughts on this experience"
+                    value={thoughts}
+                    onChange={event => { 
+                        setThoughts(event.target.value); 
+                    }}
+                    autoFocus
+                />
+                <br />
+                </div>
+
+                <div className="form-group">
+                <label htmlFor="link" className="float-left required">
+                    Link to project
+                </label>
+                <input 
+                    type="text"
+                    className="form-control"
+                    id="link"
+                    placeholder="Link to the project"
+                    value={link}
+                    onChange={event => { 
+                        setLink(event.target.value); 
+                    }}
+                    autoFocus
+                />
+                <br />
+                </div>
             </form>
         </>
     )

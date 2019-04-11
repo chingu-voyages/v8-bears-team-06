e83d96659c7fcfae3c67ef17e36fc32f623d85ba
Otsuki Hitoshi <hitochan777@gmail.com>
2019-04-11 15:05:36 +0900

    Make GraphQL requests by loggedin users work with SSR

--- client/lib/init-apollo.js
+++ client/lib/init-apollo.js
@@ -31,13 +31,13 @@ function create(initialState) {
         }
       }),
       setContext((_, { headers }) => {
-        // get the authentication token from local storage if it exists
         const token = Cookie.get("token");
         // return the headers to the context so httpLink can read them
         return {
           headers: {
             ...headers,
-            authorization: token ? `Bearer ${token}` : ""
+            authorization:
+              headers.authorization || (token ? `Bearer ${token}` : "")
           }
         };
       }),

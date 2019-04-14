0f270222058ddc9016d2af4509f51f510d60cfbd
Will Ward <will.j.ward3@gmail.com>
2019-04-14 19:59:07 +0900

    added mock profile data

--- server/db.js
+++ server/db.js
@@ -2,12 +2,34 @@ import mongoose from "mongoose";
 import MongoMemoryServer from "mongodb-memory-server";
 
 import { User } from "./models/user";
+import { Profile } from "./models/profile";
 
 export async function insertMockData() {
   await User.insertMany([
     { email: "foo@foo.com", password: "foo" },
     { email: "bar@bar.com", password: "bar" }
   ]);
+  await Profile.create({
+    email: "foo@foo.com",
+    firstName: "Michael",
+    lastName: "Bolton",
+    location: "Isle of Man",
+    workType: "information technology",
+    skills: [
+      "javascript",
+      "node.js",
+      "react",
+      "web-development",
+      "html",
+      "css"
+    ],
+    tagline:
+      "A junior software developer looking to put my technical and creative skills to use in order to make something good for others.",
+    statement:
+      "I want to make a change in the world, and I believe one of the greatest drivers of change is technological innovation. I want to create innovative software that will help all who use it.",
+    experience:
+      "I did a summer internship at Microsoft headquarters in Seattle last year."
+  });
 }
 
 export async function setupDbConnection(dev, mongoUri = "") {

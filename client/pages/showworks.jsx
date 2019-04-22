5f09acb63e5f526aca91a90e8ef1c98f7ba026e6
Allan Applebee <allan.applebee@gmail.com>
2019-04-22 13:38:10 +1000

    refactor of showworks

--- client/pages/showworks.jsx
+++ client/pages/showworks.jsx
@@ -1,31 +1,41 @@
-import React from "react";
-import { ApolloConsumer } from "react-apollo";
+import React, { Component } from 'react';
+import { graphql } from "react-apollo";
+import gql from 'graphql-tag';
 
-import Layout from "../components/layouts/Layout";
-import ShowWork from "../components/ShowWork";
+const getWorksQuery = gql`
+    {
+        works {
+            title
+            link
+            id
+        }
+    }
+`;
+class ShowWorks extends Component {
+    constructor(props){
+        super(props);
+    }
+    displayWorks(){
+        var data = this.props.data;
+        if(data.loading){
+            return( <div>Loading...</div> );
+        } else {
+            return data.works.map(work => {
+                return(
+                    <li key={ work.id }>{ work.title }</li>
+                );
+            })
+        }
+    }
+    render(){
+        return(
+            <div>
+                <ul id="work-list">
+                    { this.displayWorks() }
+                </ul>
+            </div>
+        );
+    }
+}
 
-const ListWork = () => {
-  return (
-    <ApolloConsumer>
-      {client => (
-        <Layout>
-          <div className="box-container">
-            <ShowWork apolloClient={client} />
-          </div>
-          <style jsx>{`
-            .box-container {
-              text-align: center;
-              width: 100%;
-              max-width: 400px;
-              margin: auto;
-              padding-top: 40px;
-              padding-bottom: 40px;
-            }
-          `}</style>
-        </Layout>
-      )}
-    </ApolloConsumer>
-  );
-};
-
-export default ListWork;
+export default graphql(getWorksQuery)(ShowWorks);

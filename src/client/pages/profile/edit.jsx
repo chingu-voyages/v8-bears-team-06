961e2a75665887408a740ef96a13dbcd2274a639
Will Ward <will.j.ward3@gmail.com>
2019-04-22 12:47:20 +0900

    deleted commented out sections of edit and index files

--- src/client/pages/profile/edit.jsx
+++ src/client/pages/profile/edit.jsx
@@ -306,35 +306,3 @@ const EditPage = props => {
 };
 
 export default EditPage;
-
-// import React from "react";
-// import { ApolloConsumer } from "react-apollo";
-
-// import Layout from "@/client/components/layouts/Layout";
-// import EditProfileBox from "@/client/components/EditProfileBox";
-
-// const EditProfile = () => {
-//   return (
-//     <ApolloConsumer>
-//       {client => (
-//         <Layout>
-//           <div className="box-container">
-//             <EditProfileBox apolloClient={client} />
-//           </div>
-//           <style jsx>{`
-//             .box-container {
-//               text-align: center;
-//               width: 100%;
-//               max-width: 600px;
-//               margin: auto;
-//               padding-top: 40px;
-//               padding-bottom: 40px;
-//             }
-//           `}</style>
-//         </Layout>
-//       )}
-//     </ApolloConsumer>
-//   );
-// };
-
-// export default EditProfile;

dac8baf0409acf5bf730d3759fd4b397fc048249
Will Ward <will.j.ward3@gmail.com>
2019-04-25 00:02:42 +0900

    Added background image to signup page and changed main heading

--- src/client/pages/signup.jsx
+++ src/client/pages/signup.jsx
@@ -9,17 +9,40 @@ const Signup = () => {
     <ApolloConsumer>
       {client => (
         <Layout>
-          <div className="box-container">
-            <SignUpBox apolloClient={client} />
+          <div className="bg">
+            <div className="box-container">
+              <SignUpBox apolloClient={client} />
+            </div>
           </div>
+
           <style jsx>{`
             .box-container {
               text-align: center;
+              color: white;
               width: 100%;
               max-width: 400px;
               margin: auto;
               padding-top: 40px;
               padding-bottom: 40px;
+            }
+
+            .bg {
+              // display: flex;
+              // flex-direction: column;
+              background-image: linear-gradient(
+                  rgba(0, 0, 0, 0.6),
+                  rgba(0, 0, 0, 0.4)
+                ),
+                url(../static/mensShelter.jpg);
+              height: 100vh;
+              min-height: 500px;
+              width: 100vw;
+              background-position: center;
+              background-repeat: no-repeat;
+              background-size: cover;
+              position: relative;
+              justify-content: center;
+              text-align: center;
             }
           `}</style>
         </Layout>

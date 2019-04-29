a9a774ae1018e14506d9320386f4c1ca3c546e8d
Otsuki Hitoshi <hitochan777@gmail.com>
2019-04-29 23:23:00 +0900

    Improve UX of search feature

--- src/client/pages/search.jsx
+++ src/client/pages/search.jsx
@@ -20,7 +20,7 @@ export const SEARCH_WORKS = gql`
 const WorkCard = ({ id, title, description }) => (
   <div className="card" style={{ width: "18rem" }}>
     <div className="card-body">
-      <h5 className="card-title">title</h5>
+      <h5 className="card-title">{title}</h5>
       <p className="card-text">{description}</p>
       <routes.Link route="work" params={{ id }}>
         <a className="btn btn-primary">See more</a>
@@ -34,37 +34,48 @@ const SearchPage = ({ router }) => {
   return (
     <>
       <Layout>
-        <Query query={SEARCH_WORKS} variables={{ query }}>
-          {({ data, loading, error }) => {
-            if (loading) {
-              return <div>Loading...</div>;
-            }
-            if (error) {
-              return <div>Unexpected Error</div>;
-            }
-            if (data.searchWorks.length === 0) {
-              return <Alert variant="info">No matching works found</Alert>;
-            }
-            return (
-              <ul>
-                {data.searchWorks.map(work => (
-                  <li key={work.id} className="work-card-item">
-                    <WorkCard
-                      id={work.id}
-                      title={work.title}
-                      description={work.description}
-                    />
-                  </li>
-                ))}
-              </ul>
-            );
-          }}
-        </Query>
+        <div className="mt-3">
+          <Query query={SEARCH_WORKS} variables={{ query }}>
+            {({ data, loading, error }) => {
+              if (loading) {
+                return <div>Loading...</div>;
+              }
+              if (error) {
+                return <div>Unexpected Error</div>;
+              }
+              if (data.searchWorks.length === 0) {
+                return (
+                  <div className="alert-wrapper">
+                    <Alert variant="info">No matching works found</Alert>
+                  </div>
+                );
+              }
+              return (
+                <ul className="work-card-list">
+                  {data.searchWorks.map(work => (
+                    <li key={work.id}>
+                      <WorkCard
+                        id={work.id}
+                        title={work.title}
+                        description={work.description}
+                      />
+                    </li>
+                  ))}
+                </ul>
+              );
+            }}
+          </Query>
+        </div>
       </Layout>
       <style jsx>
         {`
-          .work-card-item {
+          .work-card-list {
             list-style: none;
+          }
+          .alert-wrapper {
+            display: flex;
+            justify-content: center;
+            width: 100%;
           }
         `}
       </style>

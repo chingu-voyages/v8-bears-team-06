574936ab2cb6551a156c17bc2af10e72b5621722
Will Ward <will.j.ward3@gmail.com>
2019-04-29 14:38:04 +0900

    added new queries

--- src/client/pages/profile/queries.js
+++ src/client/pages/profile/queries.js
@@ -15,3 +15,35 @@ export const GET_PROFILE = gql`
     }
   }
 `;
+
+export const GET_USER_PROFILE = gql`
+  query profile($email: String!) {
+    profile(email: $email) {
+      email
+      name
+      location
+      workType
+      skills
+      tagline
+      statement
+      experience
+      imageId
+      works {
+        id
+        title
+      }
+    }
+  }
+`;
+
+export const GET_PROFILE_CARDS = gql`
+  query profileCards {
+    profileCards {
+      name
+      workType
+      skills
+      tagline
+      imageId
+    }
+  }
+`;

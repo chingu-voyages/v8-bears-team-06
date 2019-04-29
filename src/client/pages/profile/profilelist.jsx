b6f45a9415e789f257ad03941c2cfff22e891a93
Will Ward <will.j.ward3@gmail.com>
2019-04-29 14:34:09 +0900

    added new file

---
+++ src/client/pages/profile/profilelist.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import Layout from "@/client/components/layouts/Layout";
+
+import ProfileCard from "../../components/ProfileCard";
+
+export const GET_PROFILE_CARDS = gql`
+  query profileCards {
+    profileCards {
+      name
+      workType
+      skills
+      tagline
+      imageId
+    }
+  }
+`;
+
+const ProfileList = () => {
+  return (
+    <Layout>
+      <div className="bg">
+        <div className="box-container">
+          <h2>Profiles</h2>
+          <Query query={GET_PROFILE_CARDS} fetchPolicy={"cache-and-network"}>
+            {({ data, loading, error }) => {
+              if (loading) return "Loading";
+              if (error) return <p>ERROR</p>;
+
+              return (
+                <div className="container text-center mt-3">
+                  {!data.profileCards ? (
+                    <div>
+                      <h6>Oops! Something went wrong</h6>
+                      <p>Please log out and log in again</p>
+                    </div>
+                  ) : (
+                    <div className="container profile-box text-left p-3">
+                      {data.profileCards.map((card, i) => {
+                        return (
+                          <ProfileCard
+                            key={i}
+                            name={card.name}
+                            publicId={card.imageId}
+                            tagline={card.tagline}
+                            industry={card.workType}
+                            skills={card.skills}
+                          />
+                        );
+                      })}
+                    </div>
+                  )}
+                </div>
+              );
+            }}
+          </Query>
+        </div>
+      </div>
+      <style jsx>{`
+        @import url("https://fonts.googleapis.com/css?family=Open+Sans|Raleway|Cabin|Philosopher");
+        h6 {
+          font-family: "Cabin";
+        }
+
+        h2 {
+          font-family: "Raleway";
+        }
+
+        .box-container {
+          text-align: center;
+          width: 100%;
+          max-width: 600px;
+          margin: auto;
+          padding-top: 40px;
+          padding-bottom: 40px;
+        }
+
+        .bg {
+          background-image: linear-gradient(
+              rgba(0, 0, 0, 0.3),
+              rgba(0, 0, 0, 0.3)
+            ),
+            url(../static/leone.jpg);
+          height: 100%;
+          min-height: 500px;
+          width: 100vw;
+          background-position: center;
+          background-repeat: no-repeat;
+          background-size: cover;
+          position: relative;
+          justify-content: center;
+          text-align: center;
+        }
+      `}</style>
+    </Layout>
+  );
+};
+
+export default ProfileList;
